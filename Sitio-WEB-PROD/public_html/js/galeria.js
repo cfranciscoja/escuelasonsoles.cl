1875a49e3470f26686965b4a7a7eba5eca0d3e4f
Carlos Aravena <cfranciscoj@hotmail.com>
2025-07-20 10:27:37 -0400

    inicial

---
+++ Sitio-WEB-PROD/public_html/js/galeria.js
@@ -0,0 +1,40 @@
+$(function(){
+	$('.thumbnail').viewbox();
+});
+
+
+
+/*const container = document.querySelector('#bootstrap-image-gallery');
+window.lightGallery(container, {
+    selector: '.thumbnail',
+    plugins: [
+        lgZoom,
+        lgThumbnail
+    ],
+});
+*/
+
+// build key actions
+/*
+$(document)
+  .keydown(function (e) {
+    switch (e.which) {
+      case 37: // left
+        if ((modalId.data('bs.modal') || {})._isShown && $('#show-previous-image').is(":visible")) {
+          $('#show-previous-image')
+            .click();
+        }
+        break;
+
+      case 39: // right
+        if ((modalId.data('bs.modal') || {})._isShown && $('#show-next-image').is(":visible")) {
+          $('#show-next-image')
+            .click();
+        }
+        break;
+
+      default:
+        return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+  }); */
